7415da9373b35739c241548ac84ecb53f2dbe848
Septian RM <rizkim457@gmail.com>
2023-06-03 10:05:23 +0700

    update pesan difile NotesService

--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -19,7 +19,7 @@ class NotesService {
       const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
 
       if(!isSuccess) {
-         throw new InvariantError('catatan gagal ditambahkan')
+         throw new InvariantError('Catatan gagal ditambahkan')
       }
 
       return id;
